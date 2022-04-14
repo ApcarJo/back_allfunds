57c53ff9489c1919ceb2ef5ac9675b91892c1cab
José Luis <hansokaz@gmail.com>
2022-04-14 18:53:31 +0200

    removed unnused code

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,34 +18,6 @@ class Trader {
             { email: email }
         )
     }
-
-    async modifyPassword(body) {
-
-        let user = await User.findById(body.user_id);
-
-        let oldPassword = body.oldPassword;
-
-        let password = user.password;
-
-        let verify = await bcrypt.compare(oldPassword, password);
-
-        if (!verify) {
-            throw new Error('Wrong user or password');
-        }
-
-        let newPassword = await bcrypt.hash(body.newPassword, 10);
-
-        return User.findByIdAndUpdate(
-            { _id: body.user_id },
-            { password: newPassword })
-
-    }
-
-    async deleteUser(id) {
-        return User.findByIdAndDelete(
-            { _id: id },
-        )
-    }
 }
 
 let userController = new Trader();
